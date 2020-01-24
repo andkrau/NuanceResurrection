3bb0304d9d6863ceca3829e51924789d7461b0ef
toxieainc <toxieainc@6b0bf7a3-bffa-4ecf-8aa8-0eaf4365b716>
2020-01-24 13:31:51 +0000

    replace custom OnesCount with __popcnt
    git-svn-id: https://svn.code.sf.net/p/nuance-nuon-emulator/code@35 6b0bf7a3-bffa-4ecf-8aa8-0eaf4365b716

--- Utility.h
+++ Utility.h
@@ -3,14 +3,15 @@
 
 #include "basetypes.h"
 
-uint32 OnesCount(uint32 x)
+__forceinline uint32 OnesCount(const uint32 x)
 {
-    x -= ((x >> 1) & 0x55555555);
+    return __popcnt(x);
+    /*x -= ((x >> 1) & 0x55555555);
     x = (((x >> 2) & 0x33333333) + (x & 0x33333333));
     x = (((x >> 4) + x) & 0x0f0f0f0f);
     x += (x >> 8);
     x += (x >> 16);
-    return(x & 0x0000003f);
+    return(x & 0x0000003f);*/
 }
 
 #endif
