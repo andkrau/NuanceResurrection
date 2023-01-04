8f7f2cd883649e96089ac6756e8edfeed88962d7
toxie <toxieainc@users.noreply.github.com>
2023-01-04 11:16:54 +0100

    cleanup, NFC

--- video_m32_o32.fs
+++ video_m32_o32.fs
@@ -36,91 +36,103 @@ const vec2 chromianceBias = vec2(0.5,0.5);
 const vec3 expansion = vec3(255.0/219.0,255.0/224.0,255.0/224.0);
 const mat3 ycrcb2rgb = mat3(1.000,1.402,0.000,1.000,-0.714136,-0.34413,1.000,0.000,1.772);
 
-void main()
+vec4 texture2D_main(vec2 uv_org)
 {
-vec4 bilerp[4];
-
-vec2 mainuv_org = gl_TexCoord[0].st*vec2(720.,resy)-0.499; //!! 0.499 because of precision problems if src width/height is rather small (Space Invaders XL)
-for(int i = 0; i < 4; ++i)
-{
-  vec2 mainuv = mainuv_org;
-  if(i == 1 || i == 3)
-    mainuv.x += 1.;
-  if(i > 1)
-    mainuv.y += 1.;
-  mainuv *= vec2(0.0013888889,1./resy); // 1./720
-
-  vec4 mainColor;
-  if(mainIs16bit != 0.)
+  vec4 bilerp[4];
+  for(int i = 0; i < 4; ++i)
   {
-    vec2 mainColor88 = texture2D(mainChannelSampler, mainuv).yx;
-    mainColor = vec4(texture2D(LUTSampler, mainColor88).xyz,1.0);
+    vec2 uv = uv_org;
+    if(i == 1 || i == 3)
+      uv.x += 1.;
+    if(i > 1)
+      uv.y += 1.;
+    uv *= vec2(0.0013888889,1./resy); // 1./720
+
+    vec4 mainColor;
+    if(mainIs16bit != 0.)
+    {
+      vec2 mainColor88 = texture2D(mainChannelSampler, uv).yx;
+      mainColor = vec4(texture2D(LUTSampler, mainColor88).xyz,1.0);
+    }
+    else
+      mainColor = texture2D(mainChannelSampler, uv).bgra;
+
+    mainColor.rgb -= preBias;
+    mainColor.rgb = clamp(mainColor.rgb * expansion, 0.0, 1.0);
+    mainColor.gb -= chromianceBias;
+    mainColor.rgb = clamp(mainColor.rgb * ycrcb2rgb, 0.0, 1.0);
+
+    bilerp[i] = mainColor;
   }
-  else
-    mainColor = texture2D(mainChannelSampler, mainuv).bgra;
 
-  mainColor.rgb -= preBias;
-  mainColor.rgb = clamp(mainColor.rgb * expansion, 0.0, 1.0);
-  mainColor.gb -= chromianceBias;
-  mainColor.rgb = clamp(mainColor.rgb * ycrcb2rgb, 0.0, 1.0);
-
-  bilerp[i] = mainColor;
+  vec2 bilerpw = fract(uv_org);
+  // smoothstep:
+  bilerpw = bilerpw*bilerpw*(3.-2.*bilerpw); //bilerpw = bilerpw*bilerpw*bilerpw*(bilerpw*(bilerpw*6.0-15.0)+10.0);
+  return   bilerp[0]*((1.-bilerpw.x)*(1.-bilerpw.y))
+         + bilerp[1]*(    bilerpw.x *(1.-bilerpw.y))
+         + bilerp[2]*((1.-bilerpw.x)*    bilerpw.y)
+         + bilerp[3]*(    bilerpw.x *    bilerpw.y);
 }
 
-vec2 bilerpw = fract(mainuv_org);
-// smoothstep:
-bilerpw = bilerpw*bilerpw*(3.-2.*bilerpw); //bilerpw = bilerpw*bilerpw*bilerpw*(bilerpw*(bilerpw*6.0-15.0)+10.0);
-vec4 mainColor
-             = bilerp[0]*((1.-bilerpw.x)*(1.-bilerpw.y))
-             + bilerp[1]*(    bilerpw.x *(1.-bilerpw.y))
-             + bilerp[2]*((1.-bilerpw.x)*    bilerpw.y)
-             + bilerp[3]*(    bilerpw.x *    bilerpw.y);
-
-vec2 overlayuv_org = gl_TexCoord[1].st*vec2(720.,resy)-0.499; //!! 0.499 because of precision problems if src width/height is rather small (Space Invaders XL)
-for(int i = 0; i < 4; ++i)
+vec4 texture2D_overlay(vec2 uv_org)
 {
-  vec2 overlayuv = overlayuv_org;
-  if(i == 1 || i == 3)
-    overlayuv.x += 1.;
-  if(i > 1)
-    overlayuv.y += 1.;
-  overlayuv *= vec2(0.0013888889,1./resy); // 1./720
-
-  vec4 overlayColor;
-  if(structOverlayChannelAlpha >= 0.)
+  vec4 bilerp[4];
+  for(int i = 0; i < 4; ++i)
   {
-    vec2 overlayColor88 = texture2D(overlayChannelSampler, overlayuv).yx;
-    overlayColor = vec4(texture2D(LUTSampler, overlayColor88).xyz,structOverlayChannelAlpha);
-  }
-  else
-    overlayColor = texture2D(overlayChannelSampler, overlayuv).bgra;
+    vec2 uv = uv_org;
+    if(i == 1 || i == 3)
+      uv.x += 1.;
+    if(i > 1)
+      uv.y += 1.;
+    uv *= vec2(0.0013888889,1./resy); // 1./720
 
-  if(overlayColor.rgb != vec3(0.0,0.0,0.0)) //!! necessary?! seems not
-  {
-    overlayColor.rgb -= preBias;
-    overlayColor.rgb = clamp(overlayColor.rgb * expansion, 0.0, 1.0);
-    overlayColor.gb -= chromianceBias;
-    overlayColor.rgb = clamp(overlayColor.rgb * ycrcb2rgb, 0.0, 1.0);
+    vec4 overlayColor;
+    if(structOverlayChannelAlpha >= 0.)
+    {
+      vec2 overlayColor88 = texture2D(overlayChannelSampler, uv).yx;
+      overlayColor = vec4(texture2D(LUTSampler, overlayColor88).xyz,structOverlayChannelAlpha);
+    }
+    else
+      overlayColor = texture2D(overlayChannelSampler, uv).bgra;
+
+    if(overlayColor.rgb != vec3(0.0,0.0,0.0)) //!! necessary?! seems not
+    {
+      overlayColor.rgb -= preBias;
+      overlayColor.rgb = clamp(overlayColor.rgb * expansion, 0.0, 1.0);
+      overlayColor.gb -= chromianceBias;
+      overlayColor.rgb = clamp(overlayColor.rgb * ycrcb2rgb, 0.0, 1.0);
+    }
+
+    bilerp[i] = overlayColor;
   }
 
-  bilerp[i] = overlayColor;
+  vec2 bilerpw = fract(uv_org);
+  // smoothstep:
+  bilerpw = bilerpw*bilerpw*(3.-2.*bilerpw); //bilerpw = bilerpw*bilerpw*bilerpw*(bilerpw*(bilerpw*6.0-15.0)+10.0);
+  return   bilerp[0]*((1.-bilerpw.x)*(1.-bilerpw.y))
+         + bilerp[1]*(    bilerpw.x *(1.-bilerpw.y))
+         + bilerp[2]*((1.-bilerpw.x)*    bilerpw.y)
+         + bilerp[3]*(    bilerpw.x *    bilerpw.y);
 }
 
-     bilerpw = fract(overlayuv_org);
-// smoothstep:
-bilerpw = bilerpw*bilerpw*(3.-2.*bilerpw); //bilerpw = bilerpw*bilerpw*bilerpw*(bilerpw*(bilerpw*6.0-15.0)+10.0);
-vec4 overlayColor
-             = bilerp[0]*((1.-bilerpw.x)*(1.-bilerpw.y))
-             + bilerp[1]*(    bilerpw.x *(1.-bilerpw.y))
-             + bilerp[2]*((1.-bilerpw.x)*    bilerpw.y)
-             + bilerp[3]*(    bilerpw.x *    bilerpw.y);
+vec4 texture2D_composite(vec2 mainuv, vec2 overlayuv)
+{
+  vec4 mainColor = texture2D_main(mainuv);
+  vec4 overlayColor = texture2D_overlay(overlayuv);
 
   if(overlayColor.rgb == vec3(0.0,0.0,0.0))
-    gl_FragColor = mainColor;
+    return mainColor;
   else
   {
     //On the Nuon, alpha represents transparency with 0x00 being fully opaque.
-    //color = overlayColor*(1 - overlay.a) + mainColor*(overlay.a)
-    gl_FragColor = vec4(mix(mainColor.rgb, overlayColor.rgb, 1.0 - overlayColor.a), 1.0);
+    return vec4(mix(mainColor.rgb, overlayColor.rgb, 1.0 - overlayColor.a), 1.0);
   }
+}
+
+void main()
+{
+  vec2 mainuv    = gl_TexCoord[0].st*vec2(720.,resy)-0.499; //!! 0.499 because of precision problems if src width/height is rather small (Space Invaders XL)
+  vec2 overlayuv = gl_TexCoord[1].st*vec2(720.,resy)-0.499; //!! dto.
+
+  gl_FragColor = texture2D_composite(mainuv, overlayuv);
 }
