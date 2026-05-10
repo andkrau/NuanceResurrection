f98514dbf5b4046f17149a5c8339eed3e70b6515
James Jones <atari@theinnocuous.com>
2026-05-10 12:15:42 -0700

    Fix line endings
    The git repo was initialized incorrectly such that
    text files are checked in with Windows/DOS-style
    line endings. Add a .gitattributes file to enforce
    that line endings are translated on checkout
    (Unix/Linux/MacOS/etc. users will see unix-style
    line endings, Windows users will see DOS-style
    line endings, and the repository will contain
    Unix-style line endings), and force-apply it to
    all files to get things in the proper state.

--- compat/windows.h
+++ compat/windows.h
@@ -1,6 +1,2 @@
-// Windows.h compatibility wrapper for Linux
-// Redirects to linux_compat.h
-#ifndef _COMPAT_WINDOWS_H
-#define _COMPAT_WINDOWS_H
-#include "linux_compat.h"
-#endif
+// Windows.h compatibility wrapper for Linux (capital W variant)
+#include "windows.h"
